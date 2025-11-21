3e2acf41eb62d2677d4c890e41e6d7e1e538f07b
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-11-21 13:35:38 +0100

    feat(storage): instantiate nats watcher and return registry store wrapper struct
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/storage/sbom_registry_store.go
+++ internal/storage/sbom_registry_store.go
@@ -6,12 +6,19 @@ import (
 	"log/slog"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/kubewarden/sbomscanner/api/storage/v1alpha1"
+	"github.com/nats-io/nats.go"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/generic/registry"
+
+	storagev1alpha1 "github.com/kubewarden/sbomscanner/api/storage/v1alpha1"
+)
+
+const (
+	sbomResourceSingularName = "sbom"
+	sbomResourcePluralName   = "sboms"
 )
 
 const CreateSBOMTableSQL = `
@@ -28,28 +35,35 @@ func NewSBOMStore(
 	scheme *runtime.Scheme,
 	optsGetter generic.RESTOptionsGetter,
 	db *pgxpool.Pool,
+	nc *nats.Conn,
 	logger *slog.Logger,
-) (*registry.Store, error) {
+) (*RegistryStoreWithWatcher, error) {
 	strategy := newSBOMStrategy(scheme)
+	newFunc := func() runtime.Object { return &storagev1alpha1.SBOM{} }
+	newListFunc := func() runtime.Object { return &storagev1alpha1.SBOMList{} }
 
-	newFunc := func() runtime.Object { return &v1alpha1.SBOM{} }
-	newListFunc := func() runtime.Object { return &v1alpha1.SBOMList{} }
+	watchBroadcaster := watch.NewBroadcaster(1000, watch.WaitIfChannelFull)
+	natsBroadcaster := newNatsBroadcaster(nc, sbomResourcePluralName, watchBroadcaster, TransformStripSBOM, logger)
 
-	store := &registry.Store{
+	store := &store{
+		db:          db,
+		broadcaster: natsBroadcaster,
+		table:       sbomResourcePluralName,
+		newFunc:     newFunc,
+		newListFunc: newListFunc,
+		logger:      logger.With("store", sbomResourceSingularName),
+	}
+
+	natsWatcher := newNatsWatcher(nc, sbomResourcePluralName, watchBroadcaster, store, logger)
+
+	registryStore := &registry.Store{
 		NewFunc:                   newFunc,
 		NewListFunc:               newListFunc,
 		PredicateFunc:             matcher,
-		DefaultQualifiedResource:  v1alpha1.Resource("sboms"),
-		SingularQualifiedResource: v1alpha1.Resource("sbom"),
+		DefaultQualifiedResource:  storagev1alpha1.Resource(sbomResourcePluralName),
+		SingularQualifiedResource: storagev1alpha1.Resource(sbomResourceSingularName),
 		Storage: registry.DryRunnableStorage{
-			Storage: &store{
-				db:          db,
-				broadcaster: watch.NewBroadcaster(1000, watch.WaitIfChannelFull),
-				table:       "sboms",
-				newFunc:     newFunc,
-				newListFunc: newListFunc,
-				logger:      logger.With("store", "sbom"),
-			},
+			Storage: store,
 		},
 		CreateStrategy: strategy,
 		UpdateStrategy: strategy,
@@ -58,11 +72,14 @@ func NewSBOMStore(
 	}
 
 	options := &generic.StoreOptions{RESTOptions: optsGetter, AttrFunc: getAttrs}
-	if err := store.CompleteWithOptions(options); err != nil {
+	if err := registryStore.CompleteWithOptions(options); err != nil {
 		return nil, fmt.Errorf("unable to complete store with options: %w", err)
 	}
 
-	return store, nil
+	return &RegistryStoreWithWatcher{
+		store:   registryStore,
+		watcher: natsWatcher,
+	}, nil
 }
 
 type sbomTableConvertor struct{}
@@ -74,12 +91,12 @@ func (c *sbomTableConvertor) ConvertToTable(_ context.Context, obj runtime.Objec
 	}
 
 	// Handle both single object and list
-	var sboms []v1alpha1.SBOM
+	var sboms []storagev1alpha1.SBOM
 	switch t := obj.(type) {
-	case *v1alpha1.SBOMList:
+	case *storagev1alpha1.SBOMList:
 		sboms = t.Items
-	case *v1alpha1.SBOM:
-		sboms = []v1alpha1.SBOM{*t}
+	case *storagev1alpha1.SBOM:
+		sboms = []storagev1alpha1.SBOM{*t}
 	default:
 		return nil, fmt.Errorf("unexpected type %T", obj)
 	}
