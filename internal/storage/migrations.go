9380d7503c3629a642b0d511ac95b0e1436c7397
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-12-17 13:05:54 +0100

    feat(storage): add resource version sequence to migrations
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/storage/migrations.go
+++ internal/storage/migrations.go
@@ -7,7 +7,19 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// resourceVersionSequenceName is the name of the PostgreSQL sequence used
+// to generate globally unique resource versions across all resources.
+const resourceVersionSequenceName = "resource_version_seq"
+
+// createResourceVersionSequenceSQL creates a sequence for resource versions.
+// This sequence is used to generate globally unique, monotonically increasing
+// resource versions for all Kubernetes resources stored in the database.
+const createResourceVersionSequenceSQL = `CREATE SEQUENCE IF NOT EXISTS resource_version_seq`
+
 func RunMigrations(ctx context.Context, db *pgxpool.Pool) error {
+	if _, err := db.Exec(ctx, createResourceVersionSequenceSQL); err != nil {
+		return fmt.Errorf("creating resource version sequence: %w", err)
+	}
 	if _, err := db.Exec(ctx, createImageTableSQL); err != nil {
 		return fmt.Errorf("creating image table: %w", err)
 	}
